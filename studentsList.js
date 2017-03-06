6b90cad0a05d3b89737bab7ecf6b8c5d7a600294
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-03-06 12:17:43 +0100

    Updated js and html

--- studentsList.js
+++ studentsList.js
@@ -1,15 +1,26 @@
 // Write yout Javascript code in this files
 
 var request = new XMLHttpRequest();
+var table = document.getElementById('students');
+
 request.open('GET', 'students.json', true);
+
 request.onreadystatechange = function() {
     if (request.readyState == 4) {
         if (request.status == 200) {
             var students = JSON.parse(request.responseText);
-            console.log(students);
+
+            students.forEach(function (student) {
+              let tr = document.createElement('tr');
+              tr.innerHTML = `<td>${student.firstname}</td>
+                              <td>${student.lastname}</td>`;
+              table.appendChild(tr);
+            });
+
         } else {
             dump("Erreur pendant le chargement de la page.\n");
         }
     }
 };
+
 request.send();
